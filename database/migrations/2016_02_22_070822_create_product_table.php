75dbcecb9f16bed9e2a654ee5e62d679bb0d9427
cuoool <chea.bonnak@gmail.com>
2016-03-16 16:54:51 +0700

    Update 2016_02_22_070822_create_product_table.php
    Add field `broken_items`, `discount`, `discount_in_percentage`

--- database/migrations/2016_02_22_070822_create_product_table.php
+++ database/migrations/2016_02_22_070822_create_product_table.php
@@ -17,8 +17,11 @@ class CreateProductTable extends Migration
             $table->integer('seller_id')->unsigned()->index();
             $table->string('product_name');
             $table->decimal('price')->nullable();
-            $table->string('unit_price')->default('usd');
+            $table->string('unit_price')->nullable();
+            $table->decimal('discount')->default(0);
+            $table->decimal('discount_in_percentage')->default(0);
             $table->integer('total_items')->nullable();
+            $table->integer('broken_items')->default(0);
             $table->text('review')->nullable();
             $table->integer('category_id');
             $table->timestamps();
